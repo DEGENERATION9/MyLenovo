ce8632f392ba7adfce9def7955ca08c324639cb6
moshengmao <44000812+moshengmao@users.noreply.github.com>
2021-01-08 17:02:33 +0800

    Create Order.java

---
+++ src/com/hpe/pojo/Order.java
@@ -0,0 +1,91 @@
+package com.hpe.pojo;
+
+import java.util.Date;
+
+public class Order {
+	int order_id;
+	String order_num;
+	int user_id;
+	int total_price;
+	int pay_status;
+	int pay_type;
+	Date pay_time;
+	int order_status;
+	String extra_info;
+	int is_delete;
+	Date create_time;
+	Date update_time;
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public String getOrder_num() {
+		return order_num;
+	}
+	public void setOrder_num(String order_num) {
+		this.order_num = order_num;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getTotal_price() {
+		return total_price;
+	}
+	public void setTotal_price(int total_price) {
+		this.total_price = total_price;
+	}
+	public int getPay_status() {
+		return pay_status;
+	}
+	public void setPay_status(int pay_status) {
+		this.pay_status = pay_status;
+	}
+	public int getPay_type() {
+		return pay_type;
+	}
+	public void setPay_type(int pay_type) {
+		this.pay_type = pay_type;
+	}
+	public Date getPay_time() {
+		return pay_time;
+	}
+	public void setPay_time(Date pay_time) {
+		this.pay_time = pay_time;
+	}
+	public int getOrder_status() {
+		return order_status;
+	}
+	public void setOrder_status(int order_status) {
+		this.order_status = order_status;
+	}
+	public String getExtra_info() {
+		return extra_info;
+	}
+	public void setExtra_info(String extra_info) {
+		this.extra_info = extra_info;
+	}
+	public int getIs_delete() {
+		return is_delete;
+	}
+	public void setIs_delete(int is_delete) {
+		this.is_delete = is_delete;
+	}
+	public Date getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+	public Date getUpdate_time() {
+		return update_time;
+	}
+	public void setUpdate_time(Date update_time) {
+		this.update_time = update_time;
+	}
+	
+}
